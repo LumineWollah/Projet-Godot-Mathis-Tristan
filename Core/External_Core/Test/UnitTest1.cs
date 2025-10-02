68cb09d51fb19a0e26cf1e36427cf3f2a74ab075
LumineWollah <mathis.vareilles@yahoo.com>
2025-10-02 11:09:03 +0200

    [ADD] Unit tests

--- Core/External_Core/Test/UnitTest1.cs
+++ Core/External_Core/Test/UnitTest1.cs
@@ -1,15 +1,45 @@
-namespace Test;
+using ConnectFour.Core;
+using ConnectFour.Logic;
 
-public class Tests
+namespace ConnectFour.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class GameTests
     {
-    }
+        [Test]
+        public void Player1WinsHorizontally()
+        {
+            var p1 = new Player("P1", 'X');
+            var p2 = new Player("P2", 'O');
+            var game = new Game(p1, p2);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            // Place 4 discs in row 5, cols 0-3
+            for (int col = 0; col < 4; col++)
+            {
+                game.Board.PlaceDisc(col, p1.Disc, out int row);
+            }
+
+            bool win = Rules.CheckWin(game.Board, 5, 3);
+            Assert.True(win, "Player1 should have won with 4 in a row horizontally.");
+        }
+        
+        [Test]
+        public void FullGameSimulation()
+        {
+            var p1 = new Player("P1", 'X');
+            var p2 = new Player("P2", 'O');
+            var game = new Game(p1, p2);
+
+            // Simulate moves
+            game.Board.PlaceDisc(0, p1.Disc, out _);
+            game.Board.PlaceDisc(0, p2.Disc, out _);
+            game.Board.PlaceDisc(1, p1.Disc, out _);
+            game.Board.PlaceDisc(1, p2.Disc, out _);
+            game.Board.PlaceDisc(2, p1.Disc, out _);
+            game.Board.PlaceDisc(2, p2.Disc, out _);
+            game.Board.PlaceDisc(3, p1.Disc, out int row);
+
+            Assert.True(Rules.CheckWin(game.Board, row, 3), "P1 should win with 4 in a row.");
+        }
+
     }
 }
