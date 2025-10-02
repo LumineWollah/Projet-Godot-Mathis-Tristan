aaced3dc972f025819f1c503565d740fd481bed7
LumineWollah <mathis.vareilles@yahoo.com>
2025-10-02 11:37:25 +0200

    [ADD] Console "engine"

---
+++ Core/External_Core/Console/ConsoleRenderer.cs
@@ -0,0 +1,25 @@
+﻿namespace ConnectFour.Console;
+using ConnectFour.Core;
+using System;
+
+public class ConsoleRenderer
+{
+    private Board board;
+
+    public ConsoleRenderer(Board board) => this.board = board;
+
+    public void Draw()
+    {
+        Console.Clear();
+        for (int r = 0; r < Board.Rows; r++)
+        {
+            for (int c = 0; c < Board.Cols; c++)
+            {
+                var disc = board.GetCell(r, c);
+                Console.Write(disc == null ? ". " : disc.Symbol + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("0 1 2 3 4 5 6");
+    }
+}
---
+++ Core/External_Core/Console/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConnectFour.Console;
+using ConnectFour.Core;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var p1 = new Player("Player 1", 'X');
+        var p2 = new Player("Player 2", 'O');
+        var game = new Game(p1, p2);
+
+        var renderer = new ConsoleRenderer(game.Board);
+        var input = new InputHandler();
+
+        bool running = true;
+        while (running)
+        {
+            renderer.Draw();
+            int col = input.GetColumnChoice(game.CurrentPlayer);
+
+            if (game.Board.PlaceDisc(col, game.CurrentPlayer.Disc, out int row))
+            {
+                if (Rules.CheckWin(game.Board, row, col))
+                {
+                    renderer.Draw();
+                    Console.WriteLine($"{game.CurrentPlayer.Name} wins!");
+                    running = false;
+                }
+                else
+                {
+                    game.SwitchTurn();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Column full. Try again.");
+            }
+        }
+    }
+}
